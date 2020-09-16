9ad63862dda28a702637af610b346a9d492ba1c1
as10rt23 <as10rt23@gmail.com>
2020-09-17 05:12:17 +0900

    test2

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Dynamic;
+
+public class animal
+{
+    int head = 1;
+    int leg;
+}
+public class bird : animal
+{
+    int leg = 2;
+    int wing = 2;
+    bool canfly;
+}
 
 namespace Test
 {
     class Program
     {
+        bird chicken = new bird();
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine();
         }
     }
 }
